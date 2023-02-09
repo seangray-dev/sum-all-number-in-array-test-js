bc833a9260500737ffd5049e5e4a52bceb80d562
Sean Gray <seangray.dev@gmail.com>
2023-02-09 02:04:21 +0000

    solved recursive function

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum = 0;
+  for (num of array) {
+    if (!Array.isArray(num)) {
+      sum += num;
+    } else {
+      sum += sumItems(num);
+    }
+  }
+  return sum;
 }
 
+// console.log(sumItems([1, 2, 3, 4, 5]));
+console.log(sumItems([[[5], [[[4]]], [[3], 2]], 1, []])); // 15
 module.exports = sumItems;
